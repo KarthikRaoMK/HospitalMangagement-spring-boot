daf34aeca1b1b43bded6c417fd0855099d68c497
Karthik <raokarthik946@gmail.com>
2026-01-08 19:09:23 +0530

    Service layer added

---
+++ hospitalmangement/src/main/java/com/example/hospitalmangement/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.example.hospitalmangement.service;
+
+
+import com.example.hospitalmangement.entity.Patient;
+import com.example.hospitalmangement.repository.Patientrepository;
+
+public class PatientService {
+    private final Patientrepository repository;
+
+    public PatientService(Patientrepository repository) {
+        this.repository = repository;
+    }
+    //CREAT
+    public Patient savePatient(Patient patient){
+        return repository.save(patient);
+    }
+    //READ
+    public List<Patient> getAllPatients(){
+        return repository.findAll();
+    }
+
+
+}
